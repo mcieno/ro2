73e720aea0af77d80680353a94428813e234d205
mcieno <cieno.marco@gmail.com>
2020-03-19 23:13:49 +0100

    refactor(tspplot): remove temporary file after plotting

--- src/tspplot.c
+++ src/tspplot.c
@@ -13,34 +13,26 @@
 
 
 void
-plot_instance ( instance *problem )
+instance_to_plot_dat ( instance *problem, char *outputfile )
 {
-    FILE * plotfd = fopen( tspplotfile, "w" );
+    FILE * fd = fopen( outputfile, "w" );
 
     for ( unsigned long i = 0; i < problem->nnodes; ++i ) {
-        fprintf( plotfd, "%lu %lf %lf \n", i + 1, problem->xcoord[i], problem->ycoord[i] );
+        fprintf( fd, "%lu %lf %lf \n", i + 1, problem->xcoord[i], problem->ycoord[i] );
     }
-    fclose( plotfd );
 
-    FILE * gnuplot = popen( "gnuplot -persistent", "w" );
-
-    fprintf( gnuplot, "set title '%s' \n", problem->name );
-    fprintf( gnuplot, "plot '%s' u 2:3 pt 2 notitle,"
-                      " '' u 2:3:1 w labels offset 0.7,0.7 notitle \n", tspplotfile );
-
-    fflush( gnuplot );
-    fclose( gnuplot );
+    fclose( fd );
 }
 
 
 void
-plot_solution ( instance *problem )
+solution_to_plot_dat ( instance *problem, char *outputfile )
 {
-    FILE * plotfd = fopen( tspplotfile, "w" );
+    FILE * fd = fopen( outputfile, "w" );
 
     for ( unsigned long i = 0; i < problem->nnodes; ++i ) {
         fprintf(
-            plotfd, "%lu %lf %lf \n", i + 1,
+            fd, "%lu %lf %lf \n", i + 1,
             problem->xcoord[problem->solution[i]],
             problem->ycoord[problem->solution[i]]
         );
@@ -48,19 +40,46 @@ plot_solution ( instance *problem )
 
     // Repeat starting to close the tour
     fprintf(
-        plotfd, "%lu %lf %lf \n", 0,
+        fd, "%lu %lf %lf \n", 0,
         problem->xcoord[problem->solution[0]],
         problem->ycoord[problem->solution[0]]
     );
 
-    fclose( plotfd );
+    fclose( fd );
+}
+
+
+void
+plot_instance ( instance *problem )
+{
+    instance_to_plot_dat( problem, tspplot_tmpfile );
+
+    FILE * gnuplot = popen( "gnuplot -persistent", "w" );
+
+    fprintf( gnuplot, "set title '%s' \n", problem->name );
+    fprintf( gnuplot, "plot '%s' u 2:3 pt 2 notitle,"
+                      " '' u 2:3:1 w labels offset 0.7,0.7 notitle \n", tspplot_tmpfile );
+
+    fflush( gnuplot );
+    fclose( gnuplot );
+
+    remove( tspplot_tmpfile );
+}
+
+
+void
+plot_solution ( instance *problem )
+{
+    solution_to_plot_dat( problem, tspplot_tmpfile );
 
     FILE * gnuplot = popen( "gnuplot -persistent", "w" );
 
     fprintf( gnuplot, "set title '%s' \n", problem->name );
     fprintf( gnuplot, "plot '%s' u 2:3 pt 2 w lines notitle,"
-                      " '' using 2:3:1 w labels offset 0.7,0.7 notitle \n", tspplotfile );
+                      " '' using 2:3:1 w labels offset 0.7,0.7 notitle \n", tspplot_tmpfile );
 
     fflush( gnuplot );
     fclose( gnuplot );
+
+    remove( tspplot_tmpfile );
 }
