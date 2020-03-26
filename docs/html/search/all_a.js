8d64dbac75b0e8d5fccafd546935fc7f047db2fc
mcieno <cieno.marco@gmail.com>
2020-03-26 21:29:54 +0100

    docs: update docs

--- docs/html/search/all_a.js
+++ docs/html/search/all_a.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['xpos',['xpos',['../tsp__solvers_8h.html#a3b548175249a6e65780b9d8e95acfa23',1,'tsp_solvers.c']]]
+  ['xpos',['xpos',['../tsp__solvers_8h.html#aa8310267ef8c6ab88748570f52dbcdc4',1,'tsp_solvers.c']]]
 ];
--- docs/html/search/functions_1.js
+++ docs/html/search/functions_1.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['compute_5fsolution_5fcost',['compute_solution_cost',['../tsp_8h.html#a75c4eaf22c5154739a1ef390c09d9344',1,'tsp.c']]]
+  ['compute_5fsolution_5fcost',['compute_solution_cost',['../tsp_8h.html#a75c4eaf22c5154739a1ef390c09d9344',1,'tsp.c']]],
+  ['cplex2subtours',['cplex2subtours',['../tsp__solvers_8h.html#a9fef20aa08c8878d0d470766eff5c446',1,'tsp_solvers.c']]]
 ];
