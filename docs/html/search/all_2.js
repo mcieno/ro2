06682092206899ecdc889624de5e62d6fd976ce4
mcieno <cieno.marco@gmail.com>
2020-04-07 22:03:44 +0200

    docs: update docs

--- docs/html/search/all_2.js
+++ docs/html/search/all_2.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['destroy_5finstance',['destroy_instance',['../tsp_8h.html#abc983799ba1837b747050699cf322d22',1,'tsp.c']]],
-  ['dummy_5fmodel',['dummy_model',['../tsp__solvers_8h.html#ad811ce2875e6c7e5b1f44420064d8c3b',1,'tsp_solvers_dummy.c']]]
+  ['dummy_5fmodel',['dummy_model',['../tsp__solvers_8h.html#ad811ce2875e6c7e5b1f44420064d8c3b',1,'tsp_solvers_dummy.c']]],
+  ['dummybb_5fmodel',['dummyBB_model',['../tsp__solvers_8h.html#a469571631741e0f4b9068cf981ecbaed',1,'tsp_solvers_dummybnb.c']]]
 ];
