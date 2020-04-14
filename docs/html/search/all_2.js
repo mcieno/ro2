39beaf051d7b08142bf6fda7ff23b71f8589eaf8
mcieno <cieno.marco@gmail.com>
2020-04-14 17:40:43 +0200

    docs: update docs

--- docs/html/search/all_2.js
+++ docs/html/search/all_2.js
@@ -2,5 +2,7 @@ var searchData=
 [
   ['destroy_5finstance',['destroy_instance',['../tsp_8h.html#abc983799ba1837b747050699cf322d22',1,'tsp.c']]],
   ['dummy_5fmodel',['dummy_model',['../tsp__solvers_8h.html#a7722a7c35aa23a040a06729e8b0d9d81',1,'tsp_solvers_dummy.c']]],
-  ['dummybb_5fmodel',['dummyBB_model',['../tsp__solvers_8h.html#a769c0a674a283434594472e148bb8312',1,'tsp_solvers_dummybb.c']]]
+  ['dummybb_5fmodel',['dummyBB_model',['../tsp__solvers_8h.html#a769c0a674a283434594472e148bb8312',1,'tsp_solvers_dummybb.c']]],
+  ['dummybbf_5fmodel',['dummyBBf_model',['../tsp__solvers_8h.html#ab5e5b1dfd6a5e683aebabdf5e14cff2d',1,'tsp_solvers_dummybbf.c']]],
+  ['dummybbm_5fmodel',['dummyBBm_model',['../tsp__solvers_8h.html#a80df53ca02aeaf3bfca8a61943e87ad0',1,'tsp_solvers_dummybbm.c']]]
 ];
