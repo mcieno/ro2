fcd366087da66941cc0fad1a186ca22d32bbfd27
mcieno <cieno.marco@gmail.com>
2020-04-24 22:42:56 +0200

    docs: update docs

--- docs/html/search/functions_2.js
+++ docs/html/search/functions_2.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['destroy_5finstance',['destroy_instance',['../tsp_8h.html#abc983799ba1837b747050699cf322d22',1,'tsp.c']]],
-  ['dummy_5fmodel',['Dummy_model',['../tsp__solvers_8h.html#a89107af986adc2dddb894d1a488ce4fd',1,'tsp_solvers_dummy.c']]]
+  ['dummy_5fmodel',['Dummy_model',['../tsp__solvers_8h.html#a89107af986adc2dddb894d1a488ce4fd',1,'tsp_solvers_Dummy.c']]]
 ];
