1380e5fe1810bd83d59984a3b84e6080b74010fe
mcieno <cieno.marco@gmail.com>
2020-04-24 22:40:03 +0200

    fix: sleep between job submssions so benchmark files have different names

--- jobsubmitter.sh
+++ jobsubmitter.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 qsub -cwd -m ea benchmark.job 21357 78986
-sleep 1
+sleep 10
 qsub -cwd -m ea benchmark.job 86874 10856
-sleep 1
+sleep 10
 qsub -cwd -m ea benchmark.job 83214 13476
 
 echo "[+] All jobs submitted"
